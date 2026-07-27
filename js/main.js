5f471ada737686abad6dc2a3afdf38d0303fe5dc
hikaru <240317643+h-web-works@users.noreply.github.com>
2026-07-28 02:07:34 +0900

    header

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(window).on("scroll", function () {
+  if ($(window).width() >= 768) {
+    if ($(window).scrollTop() > 800) {
+      $(".header").addClass("is__scrolled");
+    } else {
+      $(".header").removeClass("is__scrolled");
+    }
+  }
+  else {
+    if ($(window).scrollTop() > 601) {
+      $(".header").addClass("is__scrolled");
+    } else {
+      $(".header").removeClass("is__scrolled");
+    }
+  }
+});
+
+$("#js-hamburger").click(function () {
+  $(".hamburger").toggleClass("is__active");
+  $(".hamburger__menu").toggleClass("is__open");
+});
